cb0d1a6a3c3cf6002d2d855951e6aba85958eca9
ZenXChaos <montraydavis@gmail.com>
2017-09-04 13:14:27 -0500

    Initial Release (Revamped)
    Initial Release of SharpChain 1.0

---
+++ src/Block.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace SharpChainBlockChain
+{
+    public class SharpChainBlock
+    {
+        public SharpChainHash Hash { get; set; } // Block Hash
+        public SharpChainIndex Index{ get; set; } // Index Data
+
+        public int Height { get; set; } // Block Height
+
+        public int TimeStamp { get; set; } // Time in seconds
+
+        public string Data { get; set; } // Data
+        public string Owner { get; set; } // Block Submitted By ID
+        
+        public Dictionary<string,string> JsonData  // this.Data (JSON->Dictionary)
+        {
+            get
+            {
+                try
+                {
+                    return JsonConvert.DeserializeObject<Dictionary<string, string>>(this.Data);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+
+        public bool Inconsistent { get; set; } // Is Database Inconsistent
+        
+        public string GenerateHash(string SHA256_Master) // Generate The Hash
+        {
+            return SharpChainHash.ComputeHash(this.TimeStamp, this.Hash.PreviousValue, this.Hash.Value, this.Owner, SHA256_Master);
+        }
+
+        public SharpChainBlock(int _Time = 0, string _Hash = "", string _PrevHash = "", string _Data = "", string _DataFile = "", string _Owner = null, bool _Inconsistent = false, SharpChainIndex _Index = null) // Constructor
+        {
+            this.TimeStamp = _Time;
+            this.Hash = new SharpChainHash();
+            this.Hash.Value = _Hash;
+            this.Hash.PreviousValue = _PrevHash;
+            this.Data = _Data;
+            this.Owner = _Owner;
+            this.Inconsistent = _Inconsistent;
+            this.Index = _Index;
+            this.Height = 0;
+        }
+        
+        /* STATIC */
+        public static string HASH_ALGO = "sha256";
+        /* STATIC */
+    }
+
+}
---
+++ src/Hash.cs
@@ -0,0 +1,54 @@
+﻿using SharpChainBlockChain.Core;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SharpChainBlockChain
+{
+    public class SharpChainHash
+    {
+        public string Value { get; set; } // Hash
+        public string PreviousValue { get; set; } // Hash
+
+        public int Length // Hash Length
+        {
+
+        get
+            {
+                return Value.Length;
+            }
+        }
+
+        public SharpChainHash(string _Value = null, string _PreviousValue = null) // Constructor
+        {
+
+            if (_Value != null)
+            {
+                this.Value = _Value;
+            }
+
+            if (_PreviousValue != null)
+            {
+                this.PreviousValue = _PreviousValue;
+            }
+        }
+
+        public static string ComputeHash(int _time, string _PrevHash, string _dataS, string _Owner, string SHA256_Master) // Compute Hash for Block Data
+        {
+            string HashPrep = Convert.ToString(_time) + _PrevHash + Convert.ToString(_dataS.Length) + _dataS + Convert.ToString(_Owner.Length) + _Owner;
+
+            SHA256 sha = SHA256.Create();
+            sha.ComputeHash(Encoding.UTF8.GetBytes(HashPrep));
+
+            string newHashVal = SharpChainUtils.HashByteArray(sha.Hash);
+
+            sha = SHA256.Create();
+            sha.ComputeHash(Encoding.UTF8.GetBytes(SHA256_Master + newHashVal));
+
+            string generatedHash = SharpChainUtils.HashByteArray(sha.Hash);
+
+            return generatedHash;
+        }
+
+    }
+}
